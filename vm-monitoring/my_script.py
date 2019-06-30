e77a6813bd494746a4daac7597c14b79fd0829d8
unknown <edapd12@gmail.com>
2019-06-30 13:47:05 -0300

    add vm monitoring script

---
+++ vm-monitoring/my_script.py
@@ -0,0 +1,27 @@
+import time
+import psutil
+import socket
+import datetime
+import schedule
+from pymongo import MongoClient
+
+cliente = MongoClient('mongodb://172.17.0.2:27017/')
+
+def insert():
+    banco = cliente.redes
+    vms = banco.vms
+
+    vm = {
+        "timestamp": datetime.datetime.now(), 
+        "nome": socket.gethostname(),
+        "cpu": psutil.cpu_percent(),
+        "memoria": psutil.virtual_memory()[2]
+        }
+
+    vms.insert_one(vm).inserted_id
+
+schedule.every(1).minutes.do(insert)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
